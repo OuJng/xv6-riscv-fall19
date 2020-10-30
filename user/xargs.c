4da2c0f6be0b00661721fb4e6279090d94d1d3d4
oj <orchj@foxmail.com>
2020-10-30 00:58:16 -0700

    xargs fixed

--- user/xargs.c
+++ user/xargs.c
@@ -4,9 +4,12 @@
 
 
 int main(int argc, char *argv[]) {
-    char buffer[512];
+    char buffer[256];
     char* execArgs[MAXARG];
     int pos;
+
+    char tmp;
+    char *wp, *argp;
 
     if(argc < 2) {
         exit();         // default function = echo?
@@ -16,28 +19,54 @@ int main(int argc, char *argv[]) {
         execArgs[pos] = argv[pos + 1];
     }
 
-    char *wp;
-    wp = buffer;
-    execArgs[pos++] = wp;
+    wp = buffer;         // always point at previous character
+    argp = wp + 1;       // point at next argument
+    *wp = '\0';          // helps to determine redundant blanks at the beginning      
 
-    while(read(0, wp, sizeof(char)) > 0) {
-        if(*wp == ' ' || *wp == '\n' || *wp == '\0') {
-            *wp++ = '\0';
-            execArgs[pos++] = wp;
-        }
-        else {
-            wp++;
+    while(read(0, &tmp, sizeof(tmp)) > 0) {
+        switch(tmp) {
+            case ' ':                   // split arguments
+                if(*wp != '\0') {       // skip redundant blanks
+                    *(++wp) = '\0';
+                    execArgs[pos++] = argp;
+                    argp = wp + 1;
+                }
+                break;
+            case '\n':
+            case '\0':
+                if(*wp != '\0') {       // receive last argument
+                    *(++wp) = '\0';
+                    execArgs[pos++] = argp;
+                }
+                execArgs[pos] = 0;
+                if(fork() == 0) {
+                    exec(execArgs[0], execArgs);
+                    fprintf(2, "exec %s failed", execArgs[0]);
+                    exit();
+                }
+                pos = argc - 1;
+                wp = buffer;
+                argp = wp + 1;
+                wait();
+                *wp = '\0';
+                break;
+            default:
+                *(++wp) = tmp;
+                break;
         }
     }
-    *wp = '\0';
+
+    // EOF
+    if(*wp != '\0') {       // receive last argument
+        *(++wp) = '\0';
+        execArgs[pos++] = argp;
+    }
     execArgs[pos] = 0;
-
-    if(!fork()) {
+    if(fork() == 0) {
         exec(execArgs[0], execArgs);
+        fprintf(2, "exec %s failed", execArgs[0]);
+        exit();
     }
-    else {
-        wait();
-    }
-
+    wait();
     exit();
 }
